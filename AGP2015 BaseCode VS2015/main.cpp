2145f2e003e080964605ef82ac6429d2e2e1f0f6
Scott Howie <B00252403@studentmail.uws.ac.uk>
2016-02-23 15:08:04 +0000

    Changed to ian.

--- AGP2015 BaseCode VS2015/main.cpp
+++ AGP2015 BaseCode VS2015/main.cpp
@@ -3,7 +3,7 @@
 // Most of the OpenGL code for dealing with buffer objects, etc has been moved to a 
 // utility library, to make creation and display of mesh objects as simple as possible
 
-//Edited by the B0$$
+//Edited by the IAN
 
 // Windows specific: Uncomment the following line to open a console window for debug output
 #if _DEBUG
